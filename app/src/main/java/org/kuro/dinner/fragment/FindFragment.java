3ad7e8528fc0794ab2177e882301ac6a98dc161e
Liella <13557511525@139.com>
2022-08-05 22:07:22 +0800

    tabbar

---
+++ app/src/main/java/org/kuro/dinner/fragment/FindFragment.java
@@ -0,0 +1,43 @@
+package org.kuro.dinner.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import org.kuro.dinner.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link FindFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class FindFragment extends Fragment {
+
+
+    public FindFragment() {
+        // Required empty public constructor
+    }
+
+
+    public static FindFragment newInstance() {
+        FindFragment fragment = new FindFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_order, container, false);
+    }
+}
---
+++ app/src/main/java/org/kuro/dinner/fragment/MineFragment.java
@@ -0,0 +1,43 @@
+package org.kuro.dinner.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import org.kuro.dinner.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link MineFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class MineFragment extends Fragment {
+
+
+    public MineFragment() {
+        // Required empty public constructor
+    }
+
+
+    public static MineFragment newInstance() {
+        MineFragment fragment = new MineFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_order, container, false);
+    }
+}
