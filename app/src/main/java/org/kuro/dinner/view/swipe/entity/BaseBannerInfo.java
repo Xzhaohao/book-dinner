413c19ac43994709e3765de66819158ae0df5e6a
Liella <13557511525@139.com>
2022-08-30 19:14:54 +0800

    轮播图、头像组件

---
+++ app/src/main/java/org/kuro/dinner/view/swipe/entity/BaseBannerInfo.java
@@ -0,0 +1,14 @@
+package org.kuro.dinner.view.swipe.entity;
+
+/**
+ * author: xiaohaibin.
+ * time: 2018/12/3
+ * mail:xhb_199409@163.com
+ * github:https://github.com/xiaohaibin
+ * describe: BaseBannerInfo
+ */
+public interface BaseBannerInfo {
+    Object getSwipeUrl();
+
+    String getSwipeTitle();
+}
