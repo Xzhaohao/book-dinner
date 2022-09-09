e2ff32982cbb6e33ae892d77b18a29f5e831f14e
Liella <13557511525@139.com>
2022-09-09 08:56:40 +0800

    支付页面

---
+++ app/src/main/java/org/kuro/dinner/ui/PayActivity.java
@@ -0,0 +1,15 @@
+package org.kuro.dinner.ui;
+
+import android.os.Bundle;
+
+import org.kuro.dinner.R;
+import org.kuro.dinner.base.BaseUIActivity;
+
+public class PayActivity extends BaseUIActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pay);
+    }
+}
